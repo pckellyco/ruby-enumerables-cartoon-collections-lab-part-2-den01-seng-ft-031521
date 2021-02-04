7c7e573c1d64c6214379c8b7825c38cfe7dcacf6
Paige Kelly <pckelly.co@gmail.com>
2021-02-04 20:11:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -39,3 +39,9 @@ def find_valid_calls(planeteer_calls)
   end
 end
 
+def find_valid_calls(planeteer_calls)
+  valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
+  planeteer_calls.find do |element|
+    valid_calls.include?(element)
+  end
+end
